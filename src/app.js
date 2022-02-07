d05b92ac5012d5863dcda38a27b2fb3dba5da5f4
Stephany Cisneros <stephany@thinkful.com>
2022-02-06 20:50:38 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,27 +1,24 @@
-if (process.env.USER || process.env.USERNAME) require("dotenv").config();
-const cors = require("cors");
+if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require("cors");
+
+const theatersRouter = require('./theaters/theaters.router');
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
-const theatersRouter = require("./theaters/theaters.router");
 
-app.use(express.json());
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
+
+const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.use("/theaters", theatersRouter);
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
-app.use("/theaters", theatersRouter);
 
-app.use((req, res, next) => {
-	next({
-		status: 404,
-		message: "That page doesn't exist."
-	});
-});
-
-app.use((err, req, res, next) => {
-	const { status = 500, message = "Something went wrong on our end!" } = err;
-	res.status(status).json({ error: message });
-});
+app.use(notFound)
+app.use(errorHandler)
 
 module.exports = app;
